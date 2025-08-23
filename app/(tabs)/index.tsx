e3880d7a91b9902559eb1c27d07d13677c74e871
Pakpoom Kaewpraphal <pakpoom.kaewp@gmail.com>
2025-08-23 17:53:52 +0700

    feat: add profile image to HomeScreen component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
+      <Image
+        source={{ uri: "https://picsum.photos/200/200" }}
+        style={styles.profileImage}
+      />
       <Text>Hello, Poom!</Text>
     </View>
   );
@@ -15,5 +19,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75, // Half of width/height to make it a circle
+    marginBottom: 20, // Add space below the image
   },
 });
