4a02aef79b9eed90bced4e76740c89acd7f60db4
Pakpoom Kaewpraphal <pakpoom.kaewp@gmail.com>
2025-08-24 10:30:33 +0700

    feat: add name input functionality to HomeScreen with state management

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,29 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
 
 // Using Picsum Photos for placeholder images in development
 const PLACEHOLDER_IMAGE_URL = "https://picsum.photos/id/1/200/200";
 
 export default function HomeScreen() {
+  // Set up a state variable for hold the name
+  const [name, setName] = useState("Poom");
+
   return (
     <View style={styles.container}>
       <Image
         source={{ uri: PLACEHOLDER_IMAGE_URL }}
         style={styles.profileImage}
       />
-      <Text style={styles.nameText}>Poom</Text>
+      {/* Display the name from state */}
+      <Text style={styles.nameText}>{name}</Text>
       <Text style={styles.bioText}>React Native Developer</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        onChangeText={setName} // Update the state when text changes
+        value={name} // The vaule of the input is controlled by our state
+      />
     </View>
   );
 }
@@ -39,5 +50,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "gray",
     marginTop: 8, // Add space above the bio
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    width: "80%", // Take up 80% of the container width
+    paddingHorizontal: 10,
+    marginTop: 20,
+    borderRadius: 5,
   },
 });
